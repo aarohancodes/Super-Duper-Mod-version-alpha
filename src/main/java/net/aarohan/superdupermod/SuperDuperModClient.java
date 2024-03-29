99c362231ab505111da965dca92b95941b03cb95
am1234567890 <shar.rohini@gmail.com>
2024-03-29 12:59:52 -0700

    Initial commit

---
+++ src/main/java/net/aarohan/superdupermod/SuperDuperModClient.java
@@ -0,0 +1,10 @@
+package net.aarohan.superdupermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuperDuperModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/aarohan/superdupermod/SuperDuperModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aarohan.superdupermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SuperDuperModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
